9bc03b050bd9063376414317fec76ab2e7d4d66a
YomnaWegdan <101523679+YomnaWegdan@users.noreply.github.com>
2024-08-28 02:02:22 +0300

    Update apiFeatures.js

--- src/utilities/apiFeatures.js
+++ src/utilities/apiFeatures.js
@@ -1,43 +1,58 @@
 export class ApiFeatures {
     constructor(mongooseQuery, queryString) {
-        this.mongooseQuery = mongooseQuery
-        this.queryString = queryString
+        this.mongooseQuery = mongooseQuery;
+        this.queryString = queryString;
     }  
 
-    pagination(){
-        const page = this.queryString.page * 1 || 1
-        if (!page || page < 1) page = 1
-        let limit = 2
-        let skip = (page - 1) * limit
+    pagination() {
+        const page = parseInt(this.queryString.page, 10) || 1;
+        const limit = 2;
+        const skip = (page - 1) * limit;
 
-        this.mongooseQuery.find({}).skip(skip).limit(limit)
-        this.page = page
-        return this
+        this.mongooseQuery.skip(skip).limit(limit);
+        this.page = page;
+        return this;
     }
 
-    filter(){
-        const excludeQuery = ['page' , 'sort' , 'search' , 'select']
-        let filterQuery = {...this.queryString}
-        excludeQuery.forEach((el) => delete filterQuery[el])
-        filterQuery = JSON.parse(JSON.stringify(filterQuery) .replace(/(gte|gt|lte|lt|eq)/, match => `$${match}`))
-        this.mongooseQuery.find({}).skip(skip).limit(limit)
-        return this
+    filter() {
+        const excludeQuery = ['page', 'sort', 'search', 'select'];
+        let filterQuery = { ...this.queryString };
+        excludeQuery.forEach(el => delete filterQuery[el]);
+
+        // Convert query parameters to MongoDB operators
+        filterQuery = JSON.parse(JSON.stringify(filterQuery)
+            .replace(/\b(gte|gt|lte|lt|eq)\b/g, match => `$${match}`));
+
+        this.mongooseQuery.find(filterQuery);
+        return this;
     }
-    sort(){
-        if(this.queryString.sort){
-            this.mongooseQuery.sort(this.queryString.sort).replaceAll("," , " ")
+
+    sort() {
+        if (this.queryString.sort) {
+            const sortBy = this.queryString.sort.replace(/,/g, ' ');
+            this.mongooseQuery.sort(sortBy);
         }
+        return this;
     }
-    select(){
-        if(this.queryString.select){
-            this.mongooseQuery.select(this.queryString.select).replaceAll("," , " ")
+
+    select() {
+        if (this.queryString.select) {
+            const fields = this.queryString.select.replace(/,/g, ' ');
+            this.mongooseQuery.select(fields);
         }
+        return this;
     }
-    search(){
-        if(this.queryString.search){
+
+    search() {
+        if (this.queryString.search) {
+            const searchRegex = new RegExp(this.queryString.search, 'i');
             this.mongooseQuery.find({
-                $or:[{title:{$regex:this.queryString.search , $options:"i"}} , {description:{$regex:this.queryString.search , $options:"i"}}]})
+                $or: [
+                    { title: { $regex: searchRegex } },
+                    { description: { $regex: searchRegex } }
+                ]
+            });
         }
+        return this;
     }
-
 }

bd1016ed41567ae49c853a0d8ee472778cade861
YomnaWegdan <101523679+YomnaWegdan@users.noreply.github.com>
2024-08-28 02:36:52 +0300

    Update wishlist.validate.js

--- src/modules/wishlist/wishlist.validate.js
+++ src/modules/wishlist/wishlist.validate.js
@@ -6,14 +6,16 @@ const createWishlistValidation = {
     params:Joi.object({
         productId:generalFiled.id.required()
     }),
-    headers: generalFiled.headers.required(),
-};
+ headers: Joi.object({
+    token: Joi.string().required()  
+  }).unknown(true)};
 const deleteWishlistValidation = {
     params:Joi.object({
         id:generalFiled.id.required(),
         productId:generalFiled.id.required()
     }),
-    headers: generalFiled.headers.required(),
-}
+ headers: Joi.object({
+    token: Joi.string().required()  
+  }).unknown(true)}
 
 export{createWishlistValidation , deleteWishlistValidation }
